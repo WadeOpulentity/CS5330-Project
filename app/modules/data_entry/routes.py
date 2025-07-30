31111e429cc355a2adee91e43377ca900dec6264
gmturn <turnermclean2402@gmail.com>
2025-07-30 16:44:46 -0500

    framework for functions

--- app/modules/data_entry/routes.py
+++ app/modules/data_entry/routes.py
@@ -10,3 +10,21 @@ data_entry_bp = Blueprint('data_entry', __name__, template_folder='../../../temp
 # - add_route
 # - add_spacecraft
 # - add_flight
+
+def add_plant(db, attribute_tuple):
+  pass
+
+def add_space_station(db, attribute_tuple):
+  pass
+
+def add_spaceport(db, attribute_tuple):
+  pass
+
+def add_route(db, attribute_tuple):
+  pass
+
+def add_spacecraft(db, attribute_tuple):
+  pass
+
+def add_flight(db, attribute_tuple):
+  pass
