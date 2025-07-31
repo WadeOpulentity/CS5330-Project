8c9dbe4a8c878b6e06d42e465c4958bf8ab707e3
gmturn <turnermclean2402@gmail.com>
2025-07-31 15:51:02 -0500

    added routes for data entry for all tables

--- app/modules/data_entry/routes.py
+++ app/modules/data_entry/routes.py
@@ -11,20 +11,107 @@ data_entry_bp = Blueprint('data_entry', __name__, template_folder='../../../temp
 # - add_spacecraft
 # - add_flight
 
-def add_plant(db, attribute_tuple):
-  pass
+def add_planets(db, attribute_tuple):
+  query = "INSERT INTO planets (name, size, population) VALUES (%s, %s, %s)"
+  try:
+    with db.cursor as cursor:
+      cursor.execute(query, attribute_tuple)
+      db.commit()
+      return {"success": True}
+  
+  except Exception as e:
+    db.rollback()
+    return {
+      "success": False,
+      "error": str(e)
+    }
 
-def add_space_station(db, attribute_tuple):
-  pass
+def add_spacestations(db, attribute_tuple):
+  query = "INSERT INTO spacestations (name, orbiting_planet_id, owner_planet_id) VALUES (%s, %s, %s)"
+  try:
+    with db.cursor as cursor:
+      cursor.execute(query, attribute_tuple)
+      db.commit()
+      return {"success": True}
+  
+  except Exception as e:
+    db.rollback()
+    return {
+      "success": False,
+      "error": str(e)
+    }
 
-def add_spaceport(db, attribute_tuple):
-  pass
+def add_spaceports(db, attribute_tuple):
+  query = "INSERT INTO spaceports (name, planet_id, station_id, capacity, fee_per_seat) VALUES (%s, %s, %s, %s, %s)"
+  try:
+    with db.cursor as cursor:
+      cursor.execute(query, attribute_tuple)
+      db.commit()
+      return {"success": True}
+  
+  except Exception as e:
+    db.rollback()
+    return {
+      "success": False,
+      "error": str(e)
+    }
 
-def add_route(db, attribute_tuple):
-  pass
+def add_routes(db, attribute_tuple):
+  query = "INSERT INTO routes (origin_spaceport_id, destination_spaceport_id, distance) VALUES (%s, %s, %s)"
+  try:
+    with db.cursor as cursor:
+      cursor.execute(query, attribute_tuple)
+      db.commit()
+      return {"success": True}
+  
+  except Exception as e:
+    db.rollback()
+    return {
+      "success": False,
+      "error": str(e)
+    }
 
 def add_spacecraft(db, attribute_tuple):
-  pass
+  query = "INSERT INTO spacecraft (type_name, capacity, range_km) VALUES (%s, %s, %s)"
+  try:
+    with db.cursor as cursor:
+      cursor.execute(query, attribute_tuple)
+      db.commit()
+      return {"success": True}
+  
+  except Exception as e:
+    db.rollback()
+    return {
+      "success": False,
+      "error": str(e)
+    }
 
-def add_flight(db, attribute_tuple):
-  pass
+def add_flights(db, attribute_tuple):
+  query = "INSERT INTO flights (flight_number, route_id, spacecraft_id, departure_time, flight_time_hours) VALUES (%s, %s, %s, %s, %s)"
+  try:
+    with db.cursor as cursor:
+      cursor.execute(query, attribute_tuple)
+      db.commit()
+      return {"success": True}
+  
+  except Exception as e:
+    db.rollback()
+    return {
+      "success": False,
+      "error": str(e)
+    }
+  
+def add_flighschedules(db, attribute_tuple):
+  query = "INSERT INTO flightschedules (flight_number, day_of_week) VALUES (%s, %s)"
+  try:
+    with db.cursor as cursor:
+      cursor.execute(query, attribute_tuple)
+      db.commit()
+      return {"success": True}
+  
+  except Exception as e:
+    db.rollback()
+    return {
+      "success": False,
+      "error": str(e)
+    }
