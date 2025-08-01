083ebb640dd24338efab10b4e0705f709b2b0c54
Alyson <anfields@smu.edu>
2025-07-31 22:55:49 -0500

    re-added spaceport queries to model.py

--- app/models.py
+++ app/models.py
@@ -84,3 +84,63 @@ def get_flights_for_route_on_day(route_id, day_of_week):
         return []
     return rows
 
+def spaceport_arrivals_departures_query(spaceport_name):
+    # given a spaceport, list all arrivals and departures at that spaceport
+
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+
+    query = """SELECT origin_spaceport_id, 'Departure' AS route_type
+            FROM Routes
+            WHERE origin_spaceport_id = %s
+            
+            UNION
+
+            SELECT destination_spaceport_id, 'Arrival' AS route_type
+            FROM Routes
+            WHERE destination_spaceport_id = %s"""
+    
+    cursor.execute(query, (spaceport_name, spaceport_name))
+    
+    arrivals_departures = cursor.fetchall()
+
+    return arrivals_departures
+
+
+def date_and_spaceport_arrivals_departures_query(day_of_week, spaceport_name):
+    # given a date and spaceport, list all departures in order, all arrivals in order, and all flight details
+
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+
+    query = """SELECT *, 'Departure' AS flight_type
+            FROM Flights
+            WHERE route_id IN 
+                (SELECT id
+                 FROM Routes
+                 WHERE origin_spaceport_id = %s)
+            AND flight_number IN
+                (SELECT flight_number
+                 FROM FlightSchedules
+                 WHERE date = %s)
+            
+            UNION
+
+            SELECT *, 'Arrival' AS flight_type
+            FROM Flights
+            WHERE route_id IN 
+                (SELECT id
+                 FROM Routes
+                 WHERE destination_spaceport_id = %s)
+            AND flight_number IN
+                (SELECT flight_number
+                 FROM FlightSchedules
+                 WHERE date = %s)
+
+            ORDER BY departure_time ASC"""
+    
+    cursor.execute(query, (spaceport_name, day_of_week, spaceport_name, day_of_week))
+    
+    date_arrivals_departures = cursor.fetchall()
+
+    return date_arrivals_departures
